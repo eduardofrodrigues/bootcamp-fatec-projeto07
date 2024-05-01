80b733d179538234ee23f029ac1d272428497b86
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-01 16:24:44 -0300

    #2 - add and endpoint to filter an student by id and return him, if found

--- alunos-backend/alunos/src/main/java/com/abutua/alunos/controllers/StudentsController.java
+++ alunos-backend/alunos/src/main/java/com/abutua/alunos/controllers/StudentsController.java
@@ -3,8 +3,12 @@ package com.abutua.alunos.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.abutua.alunos.models.Student;
 
@@ -29,5 +33,15 @@ public class StudentsController {
     @GetMapping("/students")
     public List<Student> allStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        Student student = students.stream()
+                .filter(std -> std.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
+
+        return ResponseEntity.ok(student);
     }
 }
