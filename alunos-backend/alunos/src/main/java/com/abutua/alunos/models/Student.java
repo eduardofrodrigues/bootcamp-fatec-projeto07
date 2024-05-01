0ec7d713e6ed33da902af83b6e800a2d6563d84c
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-01 16:07:48 -0300

    #2 - add student and course models

---
+++ alunos-backend/alunos/src/main/java/com/abutua/alunos/models/Student.java
@@ -0,0 +1,71 @@
+package com.abutua.alunos.models;
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private int IdCourse;
+    private int period;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String email, String phone, int idCourse, int period) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        IdCourse = idCourse;
+        this.period = period;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getIdCourse() {
+        return IdCourse;
+    }
+
+    public void setIdCourse(int idCourse) {
+        IdCourse = idCourse;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+}
