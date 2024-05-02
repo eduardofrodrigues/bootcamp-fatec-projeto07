da3265da63924c7ff56087ba7ca89f48009a2939
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-02 15:23:18 -0300

    #2 - add an endpoint to create a student

--- alunos-backend/alunos/src/main/java/com/abutua/alunos/controllers/StudentsController.java
+++ alunos-backend/alunos/src/main/java/com/abutua/alunos/controllers/StudentsController.java
@@ -1,5 +1,6 @@
 package com.abutua.alunos.controllers;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.alunos.models.Student;
 
@@ -43,5 +47,19 @@ public class StudentsController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
 
         return ResponseEntity.ok(student);
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+        student.setId(students.size() + 1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(student.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(student);
     }
 }
